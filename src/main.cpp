228f36378c9ed877948be002a2e9db631c42f82e
Lvova Alina <37267494+AlinaLvova@users.noreply.github.com>
2018-09-20 02:32:42 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,16 @@ int main()
     double radius = 2.3, high = 4;
     int n = 100;
     std::cout << "Hello, this is STL!" << std::endl;
+
     makeTriangleStl("Triangle.stl");
     makeTetrahedronStl("Tetrahedron.stl");
     makeCylinderStl("Cylinder.stl", 100, 2.3, 4);
-    makeRingStl("Ring.stl", 40, 2);
+    makeKonusIntegration("KonusI.stl", 10, 2, 2);
+
+    std::ofstream fout("Сircles.stl");
+    makeCircleStl(fout, 15, 4, 0, 0, 0);
+    makeCircleStl(fout, 150, 8, 10, -5, 6);
+    fout.close();
+    
     return 0;
 }
